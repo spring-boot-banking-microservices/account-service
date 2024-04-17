6508083bd7eb560a28f11f6a3c45565afd19eb15
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-04-17 23:23:12 +0530

    Add AccountMapper
    - Added `AccountMapper` class to handle mapping between `Account` and `AccountDto` objects.
    - Implemented `mapToAccountDto` method in `AccountMapper` to convert `Account` objects to `AccountDto`.
    - Implemented `mapToAccount` method in `AccountMapper` to convert `AccountDto` objects to `Account`.

---
+++ src/main/java/org/example/accountservice/mapper/AccountMapper.java
@@ -0,0 +1,22 @@
+package org.example.accountservice.mapper;
+
+import org.example.accountservice.dto.AccountDto;
+import org.example.accountservice.entity.Account;
+
+public class AccountMapper {
+  public static AccountDto mapToAccountDto(Account account) {
+    return new AccountDto(
+            account.getAccountNumber(),
+            account.getAccountType(),
+            account.getBranchAddress()
+    );
+  }
+
+  public static Account mapToAccount(AccountDto accountDto, Account account) {
+    account.setAccountNumber(accountDto.accountNumber());
+    account.setAccountType(accountDto.accountType());
+    account.setBranchAddress(accountDto.branchAddress());
+
+    return account;
+  }
+}
