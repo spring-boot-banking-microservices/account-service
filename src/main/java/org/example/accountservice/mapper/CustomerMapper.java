0523feaf95e222746aa93e55de36fbb1fec1f7f8
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-04-18 19:27:27 +0530

    Add CustomerMapper
    - Added `CustomerMapper` class to handle mapping between `Customer` and `CustomerDto` objects.
    - Implemented `mapToCustomerDto` method in `CustomerMapper` to convert `Customer` objects to `CustomerDto`.
    - Implemented `mapToCustomer` method in `CustomerMapper` to convert `CustomerDto` objects to `Customer`.

---
+++ src/main/java/org/example/accountservice/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package org.example.accountservice.mapper;
+
+import org.example.accountservice.dto.CustomerDto;
+import org.example.accountservice.entity.Customer;
+
+public class CustomerMapper {
+  public static CustomerDto mapToCustomerDto(Customer customer) {
+    return new CustomerDto(
+            customer.getName(),
+            customer.getEmail(),
+            customer.getMobileNumber()
+    );
+  }
+
+  public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+    customer.setName(customerDto.name());
+    customer.setEmail(customerDto.email());
+    customer.setMobileNumber(customerDto.mobileNumber());
+
+    return customer;
+  }
+}
