afbe5ae0b3a063b264ac233c339c31149cf4812b
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-04-21 22:25:37 +0530

    Add Customer mobile number validation during account creation
    - Added a new method `findByMobileNumber` to the `CustomerRepository` interface.
    - Added validation in `createAccount` method of `AccountService` to check for existing customers based on mobile number.
    - Created a new method `validateCustomerDoesNotExist` to handle the validation logic.
    - Throw `CustomerAlreadyExistsException` if a customer is found with the provided mobile number.

--- src/main/java/org/example/accountservice/repository/CustomerRepository.java
+++ src/main/java/org/example/accountservice/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package org.example.accountservice.repository;
 import org.example.accountservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  Optional<Customer> findByMobileNumber(String mobileNumber);
 }
