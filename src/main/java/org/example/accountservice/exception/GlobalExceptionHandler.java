7c64ef700792c2dbd1faf5c1463e7ebeba25d094
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-05-02 23:20:59 +0530

    Add custom exception handling for validation errors
    This commit introduces the following changes to the codebase:
    
    - Extended the `GlobalExceptionHandler` class to inherit from `ResponseEntityExceptionHandler` to handle validation exceptions.
    - Override the `handleMethodArgumentNotValid` method to handle `MethodArgumentNotValidException` exceptions.
    - Inside the `handleMethodArgumentNotValid` method:
     - Created a `Map<String, String>` to store the field errors and their corresponding error messages.
     - Iterated over the `FieldError` objects from the `BindingResult` of the exception.
     - Added each field name and error message to the `errors` map.
     - Returned a `ResponseEntity` with the `errors` map as the response body and an HTTP status of `BAD_REQUEST`.

--- src/main/java/org/example/accountservice/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/accountservice/exception/GlobalExceptionHandler.java
@@ -1,16 +1,23 @@
 package org.example.accountservice.exception;
 
+import lombok.NonNull;
 import org.example.accountservice.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponseDto> handleGlobalException(
           Exception exception,
@@ -54,5 +61,20 @@ public class GlobalExceptionHandler {
     );
 
     return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+          @NonNull MethodArgumentNotValidException exception,
+          @NonNull HttpHeaders headers,
+          @NonNull HttpStatusCode status,
+          @NonNull WebRequest webRequest
+  ) {
+    Map<String, String> errors = new HashMap<>();
+
+    exception.getBindingResult().getFieldErrors().forEach(error ->
+            errors.put(error.getField(), error.getDefaultMessage()));
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
