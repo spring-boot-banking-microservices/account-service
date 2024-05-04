2832210edc0e0e625cb50edf5257e70cde74bc53
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-05-04 13:33:42 +0530

    Fix typo in auditing configuration
    This commit introduces the following changes to the codebase:
    
    - Fixed a typo in the `AccountServiceApplication` class by changing the `auditorAwareRef` attribute value from `"auditAwareImpl"` to `"auditorAwareImpl"`.
    - Fixed a typo in the `AuditorAwareImpl` class by changing the `@Component` annotation value from `"auditAwareImpl"` to `"auditorAwareImpl"`.

--- src/main/java/org/example/accountservice/audit/AuditorAwareImpl.java
+++ src/main/java/org/example/accountservice/audit/AuditorAwareImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component(value = "auditAwareImpl")
+@Component(value = "auditorAwareImpl")
 public class AuditorAwareImpl implements AuditorAware<String> {
   @Override
   @NonNull
