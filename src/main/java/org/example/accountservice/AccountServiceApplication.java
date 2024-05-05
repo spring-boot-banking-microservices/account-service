03a1a637dfe4782ee582412f12b456804dbc3a7d
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-05-05 12:28:26 +0530

    Add OpenAPI Documentation Configuration
    This commit adds the initial configuration for OpenAPI documentation to the project.
    
    Changes:
    
    - Add `springdoc-openapi-starter-webmvc-ui` dependency to `pom.xml` for generating OpenAPI documentation.
    - Add `@OpenAPIDefinition` annotation to `AccountServiceApplication` class, providing metadata about the API, such as title, version, description, contact information, and license details.

--- src/main/java/org/example/accountservice/AccountServiceApplication.java
+++ src/main/java/org/example/accountservice/AccountServiceApplication.java
@@ -1,11 +1,30 @@
 package org.example.accountservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Account Service API",
+                version = "1.0.0",
+                description = "API documentation for the Account Service",
+                contact = @Contact(
+                        name = "Aamir Shaikh",
+                        email = "aamir.email@example.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        )
+)
 public class AccountServiceApplication {
 
   public static void main(String[] args) {
